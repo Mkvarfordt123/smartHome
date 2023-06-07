a250ad4a44e8aa049b46ffa2e2633ed21330a476
MiloKvarfordt <MIlo.kvarfordt123@gmail.com>
2023-06-07 12:18:27 -0700

    finished storyboard and model

---
+++ SmartHome/SmartHome/New Group/Controllers/DeviceController.swift
@@ -0,0 +1,8 @@
+//
+//  DeviceController.swift
+//  SmartHome
+//
+//  Created by Milo Kvarfordt on 6/7/23.
+//
+
+import Foundation
---
+++ SmartHome/SmartHome/New Group/Models/Device.swift
@@ -0,0 +1,31 @@
+//
+//  Device.swift
+//  SmartHome
+//
+//  Created by Milo Kvarfordt on 6/7/23.
+//
+
+import Foundation
+
+class Device {
+ 
+    var name: String
+    var isOn: Bool
+    let uuid: UUID
+    
+    init(name: String, isOn: Bool = false, uuid: UUID = UUID()) {
+        self.name = name
+        self.isOn = isOn
+        self.uuid = uuid
+    }
+}
+
+
+ // MARK: - Extension
+extension Device: Equatable {
+    static func == (lhs: Device, rhs: Device) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+    
+    
+}
